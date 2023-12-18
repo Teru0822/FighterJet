4c3963d13ca9293707f879b81a3ef549c440e901
Teru0822 <ycam0822@gmail.com>
2023-12-18 13:54:16 +0900

    プロジェクト ファイルを追加します。

---
+++ ret.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "GL/freeglut.h"
+#define ARRAY_MAX 10000000
+
+float Rvertex[ARRAY_MAX];
+int Rlines[ARRAY_MAX];
+int RvertexDataSize = 0, RlineDataSize = 0;
+
+
+void makeRet()
+{
+    glPointSize(3);
+    glColor3f(0, 1, 1);
+    glBegin(GL_LINES);  //ê¸ÇÃï`é 
+    {
+        for (int i = 0; i < RlineDataSize; i++) {
+            glArrayElement(Rlines[i * 3]);
+            glArrayElement(Rlines[i * 3 + 1]);
+
+            glArrayElement(Rlines[i * 3 + 1]);
+            glArrayElement(Rlines[i * 3 + 2]);
+
+            glArrayElement(Rlines[i * 3 + 2]);
+            glArrayElement(Rlines[i * 3]);
+        }
+    }
+    glEnd();
+
+    glColor3f(0, 1, 1);
+    glBegin(GL_TRIANGLES); //ñ ÇÃï`é 
+    {
+        for (int i = 0; i < RlineDataSize; i++) {
+            glArrayElement(Rlines[i * 3]);   //0
+            glArrayElement(Rlines[i * 3 + 1]);  //1
+            glArrayElement(Rlines[i * 3 + 2]);  //2
+        }
+    }
+    glEnd();
+}
